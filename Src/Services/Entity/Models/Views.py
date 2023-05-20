2ea3748679fb3f5b0ca8d2414fbcdf057b817cd3
reza jenabi <jenabireza@gmail.com>
2023-05-20 15:49:08 +0430

    init views models

--- Src/Services/Entity/Models/Views.py
+++ Src/Services/Entity/Models/Views.py
@@ -1,2 +1,11 @@
 class Views:
-    pass
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def Name(self):
+        return self._name
+
+    @Name.setter
+    def Name(self, value):
+        self._name = value
