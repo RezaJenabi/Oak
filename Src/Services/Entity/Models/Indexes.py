a9856d6e1dfcdeedffcb094d9b01b3c1feb88e92
reza jenabi <jenabireza@gmail.com>
2023-05-20 15:40:50 +0430

    create indexes models

--- Src/Services/Entity/Models/Indexes.py
+++ Src/Services/Entity/Models/Indexes.py
@@ -1,4 +1,9 @@
 class Indexed:
+    def __init__(self, name, sourceName, indexType):
+        self._indexType = indexType
+        self._sourceName = sourceName
+        self._name = name
+
     @property
     def Name(self):
         return self._name
@@ -6,3 +11,19 @@ class Indexed:
     @Name.setter
     def Name(self, value):
         self._name = value
+
+    @property
+    def SourceName(self):
+        return self._sourceName
+
+    @SourceName.setter
+    def SourceName(self, value):
+        self._sourceName = value
+
+    @property
+    def IndexType(self):
+        return self._indexType
+
+    @IndexType.setter
+    def IndexType(self, value):
+        self._indexType = value
